296dc1d92ea18b35eae65d8031c58e60a87c039a
DavitEgoian <dato.egoyan@gmail.com>
2025-05-01 15:08:18 +0400

    Main file added with imports

---
+++ main.py
@@ -0,0 +1,6 @@
+import sys, os
+from datetime import datetime
+from PyQt5 import QtWidgets, QtGui, QtCore
+from color_extractor import ColorExtractor
+from palette_saver import PaletteSaver
+from PIL import Image
