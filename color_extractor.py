97b579e9507242dcc9577da576a6207cab1dbf29
DavitEgoian <dato.egoyan@gmail.com>
2025-05-01 15:11:43 +0400

    Color extractor logic added

--- color_extractor.py
+++ color_extractor.py
@@ -2,4 +2,10 @@ from PIL import Image
 from collections import Counter
 
 class ColorExtractor:
-    pass
+    def __init__(self, image: Image.Image) -> None:
+        self.image = image
+
+    def top_colors(self, num: int = 10) -> list[tuple[int, int, int]]:
+        pixels = self.image.convert('RGB').getdata()
+        counter = Counter(pixels)
+        return [color for color, _ in counter.most_common(num)]
